68ab8e100beeb010f3a8c7714c5a570b1a49aa1d
JeremyJamesL <jeremyluscombe@gmail.com>
2024-09-05 21:03:45 +0100

    "basic fetches"

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,46 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 8080;
 const { Chess } = require("chess.js");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Mongoose
+mongoose.connect("mongodb://localhost:27017/chess").then(() => {
+  console.log("db connection established");
+});
+
+const gameSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Game must have a name"],
+    trim: true,
+  },
+  gameURL: {
+    type: String,
+    required: [true, "Game must have a URL"],
+    trim: true,
+    unique: true,
+  },
+  side: {
+    type: String,
+    required: [true, "Game must have a name"],
+  },
+  moves: {
+    type: [String],
+    required: [true, "Game must have moves"],
+  },
+  pgn: {
+    type: String,
+    required: [true, "Game must have a pgn"],
+  },
+});
+
+const Game = mongoose.model("games", gameSchema);
+
+// Routes
 app.get("/get-eval", async (req, res) => {
   const response = await fetch("https://chess-api.com/v1", {
     method: "POST",
@@ -23,11 +58,32 @@ app.get("/get-eval", async (req, res) => {
   res.status(200).send(data);
 });
 
-app.post("/get-fens", async (req, res) => {
+app.post("/process-game", async (req, res) => {
+  // Get basic game info
+  const theGame = req.body.game;
+  const isWhiteOrBlack =
+    theGame.white.username === req.body.username ? "white" : "black";
+
+  // Get fen steps
   const chess = new Chess();
-  chess.loadPgn(req.body.pgn);
+  chess.loadPgn(theGame.pgn);
   const history = chess.history({ verbose: true });
-  console.log(history);
+  const moves = history.map((el) => {
+    return el.before;
+  });
+
+  // Transformed object
+  const transformedGame = {
+    name: `${theGame.white.username} VS ${theGame.black.username} ${theGame.time_class} game`,
+    gameURL: theGame.url,
+    side: isWhiteOrBlack,
+    moves,
+    pgn: theGame.pgn,
+  };
+
+  // Add to MongoDB
+  const newRecord = await Game.create(transformedGame);
+  console.log(newRecord);
 });
 
 app.listen(port, () => {
